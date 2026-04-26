1d76b532301d2bf4ef86eb998d35b779760c0244
rionadevz <rionadevz@gmail.com>
2026-04-26 20:13:53 +0700

    deploy gh page cnfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-   output: 'export',
+   output: 'export', // paksa static
+   basePath: '/sura-fe', // nama repo
+   assetPrefix: '/sura-fe/', // biar asset nggak 404
+   images: {
+      unoptimized: true, // next/image harus dimatikan optimasinya
+   },
 }
 
 export default nextConfig
