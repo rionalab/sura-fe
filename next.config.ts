aeec59f0a59d1d5c6c4bd3a9f95f0a4a82625351
rionadevz <rionadevz@gmail.com>
2026-04-26 21:27:31 +0700

    update skrip deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
    output: 'export', // paksa static
    basePath: '/sura-fe', // nama repo
-   assetPrefix: '/sura-fe/', // biar asset nggak 404
+   assetPrefix: '/sura-fe/', // biar asset nggak 404,
+   trailingSlash: true,
    images: {
       unoptimized: true, // next/image harus dimatikan optimasinya
    },
