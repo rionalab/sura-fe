13d3167c6e894b7292a248985202693fc1705560
rionadevz <rionadevz@gmail.com>
2026-04-26 20:09:22 +0700

    load landing1 on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import SetDefaultTheme from '@/src/comps/set-default-theme'
 import ThemeSwitcher from '@/src/comps/theme-switcher'
+import { Landing1Page } from '@/src/features/landing1'
 
 export default function Home() {
-   return (
+   return <Landing1Page />
+
+   /*    return (
       <div>
          <SetDefaultTheme />
          <div className="main-container">
             <ThemeSwitcher />
          </div>
       </div>
-   )
+   ) */
 }
